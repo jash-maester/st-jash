5c8ffbcf7a638e1ec3176edf75accfdf008ffe3d
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2021-07-08 18:05:09 +0530

    decreased transparency

--- config.def.h
+++ config.def.h
@@ -101,7 +101,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.85;
+float alpha = 0.87;
 
 
 /* Terminal colors (16 first used in escape sequence) */
